e30fd9de17c199209c86cf11ba8847e6dce8823d
Robin Tessier <robin.tessier@korian.fr>
2022-09-16 20:08:47 +0200

    all cases working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { inputs } from './inputs'
+import { inputs } from './data/inputs'
 import React, { useState } from 'react'
 import { convertDate, sortsDates, isOverLapping } from './utils/utils'
 
@@ -17,21 +17,18 @@ function findSlotWidth(slots, index) {
   //* first assume that a slot A overlapping with slot B and C will take 1/3 of window width
   //* then compare slots B and C to see if they overlap, if they don't then A will take 1/2 of window width
   let width = slots[index].overlapWith.length + 1
-  for (let i = 0; i < slots[index].overlapWith.length; i++) {
-    if (
-      i < slots[index].overlapWith.length - 1 &&
-      !isOverLapping(
-        slots[slots[index].overlapWith[i]],
-        slots[slots[index].overlapWith[i + 1]],
-      )
-    ) {
-      width--
-    }
+  if (
+    slots[index].overlapWith.length &&
+    slots[slots[index].overlapWith[0]].overlapWith
+  ) {
+    width = slots[slots[index].overlapWith[0]].width
   }
   return width
 }
 
 function findSlotPosition(slots, index) {
+  //* find the horizontal position for the element
+  //* then divide by the width property like so : 0/3, 1/3 and 2/3
   let position = 0
   for (let i = 0; i < slots[index].overlapWith.length; i++) {
     if (
@@ -44,7 +41,15 @@ function findSlotPosition(slots, index) {
   return position
 }
 
-function findWidthRatio(slots, index) {}
+function findWidthRatio(slots, index) {
+  //* if an element doesnt completely fill the space horizontally change his ratio
+  //* for example from 1/3 to 2/3 of the screen
+  let widthRatio = 1
+  if (slots[index].overlapWith.length < slots[index].width - 1) {
+    widthRatio = slots[index].width - slots[index].overlapWith.length
+  }
+  return widthRatio
+}
 
 function parseSlots(slots) {
   convertDate(slots)
@@ -60,13 +65,15 @@ function parseSlots(slots) {
 }
 
 export default function App() {
-  const [slots] = useState(parseSlots(inputs.slice(0, 10)))
+  const [slots] = useState(parseSlots(inputs))
 
   return (
-    <div className="p-4">
+    <div className="p-3">
+      <h1 className="text-center title font-semibold text-xl p-2">
+        React Rendering Event Senior
+      </h1>
       <div className="border border-gray-300 ml-4 py-2 rounded-sm relative h-[1800px]">
         {slots.map((el) => {
-          console.log(el)
           return (
             <div
               key={el.id}
@@ -75,11 +82,11 @@ export default function App() {
                 top: (el.timeStamp - 500) * 2.5,
                 left: el.width === 1 ? 0 : (el.position / el.width) * 100 + '%',
                 right: el.width === 1 && 0,
-                width: (1 / el.width) * 100 + '%',
+                width: (el.widthRatio / el.width) * 100 + '%',
               }}
               className="absolute bg-cyan-200 border border-cyan-500 rounded"
             >
-              {el.start + ' - ' + el.end + ' ' + el.width}
+              {el.start + ' - ' + el.end}
             </div>
           )
         })}
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,12 @@
+function convertDateToTimestamp(date) {
+  let splittedDate = date.split(':')
+  return parseInt(splittedDate[0]) * 60 + parseInt(splittedDate[1])
+}
+
 export function convertDate(slots) {
   //* calculate end time
   slots.forEach((slot) => {
-    let splitStart = slot.start.split(':')
-    let time =
-      parseInt(splitStart[0]) * 60 + parseInt(splitStart[1]) + slot.duration
+    let time = convertDateToTimestamp(slot.start) + slot.duration
     let newHour = Math.floor(time / 60).toString()
     let newMin = time % 60 === 0 ? '00' : (time % 60).toString()
     slot.end = newHour + ':' + newMin
@@ -21,9 +24,14 @@ export function sortsDates(slots) {
 
 export function isOverLapping(slot1, slot2) {
   //* return true if slot 1 and slot 2 overlap
+  let start1 = convertDateToTimestamp(slot1.start)
+  let end1 = convertDateToTimestamp(slot1.end)
+  let start2 = convertDateToTimestamp(slot2.start)
+  let end2 = convertDateToTimestamp(slot2.end)
   if (
-    (slot1.start < slot2.end && slot1.start >= slot2.start) ||
-    (slot1.end > slot2.start && slot1.end <= slot2.end)
+    (start1 < end2 && start1 >= start2) ||
+    (end1 > start2 && end1 <= end2) ||
+    (end1 >= end2 && start1 <= start2)
   ) {
     return true
   }
