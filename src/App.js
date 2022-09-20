dba74136494fbfb42093b75a4953a2f37aa3a839
Robin Tessier <robin.tessier@korian.fr>
2022-09-20 18:11:23 +0200

    moved functions to utils

--- src/App.js
+++ src/App.js
@@ -1,55 +1,6 @@
 import { inputs } from './data/inputs'
 import React, { useState } from 'react'
-import { convertDate, sortsDates, isOverLapping } from './utils/utils'
-
-function findOverlaps(slots, index) {
-  //* fill an array with indexes of overlapping elements
-  let overlapWith = []
-  for (let i = 0; i < slots.length; i++) {
-    if (i !== index && isOverLapping(slots[index], slots[i])) {
-      overlapWith.push(i)
-    }
-  }
-  return overlapWith
-}
-
-function findSlotWidth(slots, index) {
-  //* first assume that a slot A overlapping with slot B and C will take 1/3 of window width
-  //* then if the slot is overlapping with other slots, take the width of the first one by order
-  let width = slots[index].overlapWith.length + 1
-  if (
-    slots[index].overlapWith.length &&
-    slots[slots[index].overlapWith[0]].overlapWith
-  ) {
-    width = slots[slots[index].overlapWith[0]].width
-  }
-  return width
-}
-
-function findSlotPosition(slots, index) {
-  //* find the horizontal position for the element
-  //* then divide by the width property like so : 0/3, 1/3 and 2/3
-  let position = 0
-  for (let i = 0; i < slots[index].overlapWith.length; i++) {
-    if (
-      typeof slots[slots[index].overlapWith[i]].position !== 'undefined' &&
-      slots[slots[index].overlapWith[i]].position < slots[index].width - 1
-    ) {
-      position++
-    }
-  }
-  return position
-}
-
-function findWidthRatio(slots, index) {
-  //* if an element doesnt completely fill the space horizontally change his ratio
-  //* for example from 1/3 to 2/3 of the screen
-  let widthRatio = 1
-  if (slots[index].overlapWith.length < slots[index].width - 1) {
-    widthRatio = slots[index].width - slots[index].overlapWith.length
-  }
-  return widthRatio
-}
+import { convertDate, sortsDates, findOverlaps, findSlotWidth, findSlotPosition, findWidthRatio } from './utils/utils'
 
 function parseSlots(slots) {
   convertDate(slots)
@@ -72,17 +23,18 @@ export default function App() {
       <h1 className="text-center title font-semibold text-xl p-2">
         React Rendering Event Senior
       </h1>
-      <div className="border border-gray-300 ml-4 py-2 rounded-sm relative h-[1800px]">
+      <div className="border border-gray-300 ml-4 py-2 rounded-sm relative h-[1800px] bg-slate-50">
         {slots.map((el) => {
           return (
             <div
               key={el.id}
               style={{
-                height: el.duration * 2.5,
-                top: (el.timeStamp - 500) * 2.5,
+                height: el.duration * 1 ,
+                top: (el.timeStamp - 530) * 1,
                 left: el.width === 1 ? 0 : (el.position / el.width) * 100 + '%',
                 right: el.width === 1 && 0,
                 width: (el.widthRatio / el.width) * 100 + '%',
+                fontSize:'7px'
               }}
               className="absolute bg-cyan-200 border border-cyan-500 rounded"
             >
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,6 +3,22 @@ function convertDateToTimestamp(date) {
   return parseInt(splittedDate[0]) * 60 + parseInt(splittedDate[1])
 }
 
+function isOverLapping(slot1, slot2) {
+  //* return true if slot 1 and slot 2 overlap
+  let start1 = convertDateToTimestamp(slot1.start)
+  let end1 = convertDateToTimestamp(slot1.end)
+  let start2 = convertDateToTimestamp(slot2.start)
+  let end2 = convertDateToTimestamp(slot2.end)
+  if (
+    (start1 < end2 && start1 >= start2) ||
+    (end1 > start2 && end1 <= end2) ||
+    (end1 >= end2 && start1 <= start2)
+  ) {
+    return true
+  }
+  return false
+}
+
 export function convertDate(slots) {
   //* calculate end time
   slots.forEach((slot) => {
@@ -22,18 +38,51 @@ export function sortsDates(slots) {
   return slots
 }
 
-export function isOverLapping(slot1, slot2) {
-  //* return true if slot 1 and slot 2 overlap
-  let start1 = convertDateToTimestamp(slot1.start)
-  let end1 = convertDateToTimestamp(slot1.end)
-  let start2 = convertDateToTimestamp(slot2.start)
-  let end2 = convertDateToTimestamp(slot2.end)
-  if (
-    (start1 < end2 && start1 >= start2) ||
-    (end1 > start2 && end1 <= end2) ||
-    (end1 >= end2 && start1 <= start2)
-  ) {
-    return true
+export function findOverlaps(slots, index) {
+  //* fill an array with indexes of overlapping elements
+  let overlapWith = []
+  for (let i = 0; i < slots.length; i++) {
+    if (i !== index && isOverLapping(slots[index], slots[i])) {
+      overlapWith.push(i)
+    }
   }
-  return false
+  return overlapWith
+}
+
+export function findSlotWidth(slots, index) {
+  //* first assume that a slot A overlapping with slot B and C will take 1/3 of window width
+  //* then if the slot is overlapping with other slots, take the width of the first one by order
+  let width = slots[index].overlapWith.length + 1
+  if (
+    slots[index].overlapWith.length &&
+    slots[slots[index].overlapWith[0]].overlapWith
+  ) {
+    width = slots[slots[index].overlapWith[0]].width
+  }
+  return width
+}
+
+export function findSlotPosition(slots, index) {
+  //* find the horizontal position for the element
+  //* then divide by the width property like so : 0/3, 1/3 and 2/3
+  let position = 0
+  for (let i = 0; i < slots[index].overlapWith.length; i++) {
+    if (
+      typeof slots[slots[index].overlapWith[i]].position !== 'undefined' &&
+      slots[slots[index].overlapWith[i]].position < slots[index].width - 1
+    ) {
+      position++
+    }
+  }
+  return position
+}
+
+export function findWidthRatio(slots, index) {
+  //* if an element doesnt completely fill the space horizontally change his ratio
+  //* for example from 1/3 to 2/3 of the screen
+  let widthRatio = 1
+  if (slots[index].overlapWith.length < slots[index].width - 1) {
+    widthRatio = slots[index].width - slots[index].overlapWith.length
+  }
+  return widthRatio
 }
